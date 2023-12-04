a5e54883472d5daed96b3d96cb9f40990b8b5810
Sahm269 <ahmedsouraya@vaeg-juridica.com>
2023-12-04 12:09:21 +0100

    Pas de modif

--- Projet_poker/js/affichagemenu.js
+++ Projet_poker/js/affichagemenu.js
@@ -31,60 +31,46 @@ function showPart(part) {
 
 function startGame() {
    
-   //processBacklog();
-   var formulaire = document.getElementById('setupForm');
-   var formData = new FormData(formulaire);
-   var pseudo_hote = formData.get('pseudoHote')
-   var nom_projet = formData.get('nomProjet')
-   var nombre_joueur = formData.get('nombreJoueur')
-   var i = 0;
-   var tab_nom_joueur = [];
-   while(i<=nombre_joueur){
-        tab_nom_joueur[i]=formData.get('player'+i)
-        i++;
-   }
-   var tab_backlog = [];
-   var backlog = formData.get('backlog');
-   /*
-   var lignes = backlog.split('\n');
-   var nb_ligne = lignes.length;
-   var j = 0;
-   while(j<=nb_ligne){
-    tab_backlog[j]=lignes.j;
-    j++;
-   }
-   var affichage_backlog="";
-   */
-   alert("pseudo " + pseudo_hote + "nb joueur " + nombre_joueur + "liste joueur " + tab_nom_joueur + "baclog " + backlog)
+   
 
-
-   //recuperer_donnees();
+   //alert("pseudo " + pseudo_hote + "nb joueur " + nombre_joueur + "liste joueur " + tab_nom_joueur + "baclog " + backlog)
+   recuperer_donnees();
 }
 
 function recuperer_donnees(){
+
+    var formulaire = document.getElementById('setupForm');
+    var formData = new FormData(formulaire);
+    var pseudo_hote = formData.get('pseudoHote')
+    var nom_projet = formData.get('nomProjet')
+    var nombre_joueur = formData.get('nombreJoueur')
+    var i = 0;
+    var tab_nom_joueur = [];
+    while(i<=nombre_joueur){
+         tab_nom_joueur[i]=formData.get('player'+i)
+         i++;
+    }
+    var backlog = formData.get('backlog');
+ var lignes = backlog.split('\n');
+ var nb_ligne = lignes.length;
+ var backlogJson = [];
+ 
+ for (var i = 0; i < nb_ligne; i++) {
+     // Initialiser chaque ligne avec une chaîne vide
+     var tacheEstimation = { tache: lignes[i], estimation: '' };
+     // Ajouter l'objet à l'array backlogJson
+     backlogJson.push(tacheEstimation);
+ }
+ 
+ console.log(backlogJson);
+ 
+ // Convertir l'array backlogJson en une chaîne JSON
+ var backlogJsonString = JSON.stringify(backlogJson);
+ console.log(backlogJsonString);
+ 
     
 }
 
-function processBacklog() {
-    // Récupère le contenu du textarea
-    const backlogTextarea = document.getElementById('backlog');
-    const backlogContent = backlogTextarea.value;
-
-    // Sépare les lignes pour obtenir une liste de tâches
-    const backlogTasks = backlogContent.split('\n');
-
-    // Crée un tableau de tâches au format JSON avec des estimations initialisées à une chaîne vide
-    const taskList = backlogTasks.map(task => ({ task: task.trim(), estimate: "" }));
-
-    // Convertit le tableau de tâches en JSON
-    const taskListJSON = JSON.stringify(taskList, null, 2);
-    console.log(taskListJSON);
-    document.getElementById('taskList').value = taskListJSON;
-
-    // Affiche la liste de tâches JSON dans la zone de texte
-    document.getElementById('taskList').value = taskListJSON;
-
-
+function afficherDonner(){
 
 }
-
